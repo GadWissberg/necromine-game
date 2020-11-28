b27f220863944e70feda267c2c7b7b9629c947db
Gad Wissberg <gadw17@gmail.com>
2020-11-28 20:00:02 +0200

    Releasing 0.2.

--- core/src/com/gadarts/isometric/utils/DefaultGameSettings.java
+++ core/src/com/gadarts/isometric/utils/DefaultGameSettings.java
@@ -6,7 +6,7 @@ import static com.badlogic.gdx.Application.LOG_DEBUG;
 
 public class DefaultGameSettings {
 	public static final boolean DEBUG_INPUT = false;
-	public static final boolean SHOW_GL_PROFILING = false;
+	public static final boolean SHOW_GL_PROFILING = true;
 	public static final int LOG_LEVEL = LOG_DEBUG;
 	public static final boolean HIDE_GROUND = false;
 	public static final boolean HIDE_ENEMIES = false;
